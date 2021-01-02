90987de032743e173e5b4d132ec0d86ecbe7a6c6
Bruno Martins <b.gabriel.ma.dev@gmail.com>
2021-01-02 15:48:05 +0000

    feat: 🎸 firt
    created first example program

---
+++ learning/fundamentals/first/first.go
@@ -0,0 +1,16 @@
+// for executable programs created
+package main
+
+/*
+IO formatted
+We can use as well:
+	import (
+		"...packages"
+	)
+*/
+import "fmt"
+
+func main() {
+	fmt.Print("First ")
+	fmt.Print("Program")
+}
